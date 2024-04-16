9b10fd13476b99eda96ff9fe6cd616a38f9155ce
Val2510 <valery251094@gmail.com>
2024-04-16 18:22:49 +0100

    first commit

---
+++ hw.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Укажите полный путь до файла вторым аргументом")
+	}
+
+	filePth := os.Args[1]
+
+	var fileName, fileExt string
+	fileName = filepath.Base(filePth)
+	fileExt = strings.TrimPrefix(filepath.Ext(filePth), ".")
+
+	fmt.Printf("filename: %s\n", fileName)
+	fmt.Printf("extension: %s\n", fileExt)
+}
